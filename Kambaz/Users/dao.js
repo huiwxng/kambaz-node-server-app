51ebce0e32cebdb12455bec30d12b3cc24fe4738
hui wang <hui@huibook-pro.local>
2025-12-03 02:01:14 -0500

    6.3 commit

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,26 +1,40 @@
+import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
-
-function UsersDao(db) {
-	let { users } = db;
+export default function UsersDao(db) {
 	const createUser = (user) => {
 		const newUser = { ...user, _id: uuidv4() };
-		users = [...users, newUser];
-		return newUser;
+		return model.create(newUser);
 	};
-	const findAllUsers = () => users;
-	const findUserById = (userId) => users.find((user) => user._id === userId);
+	
+	const findUsersByRole = (role) => model.find({ role: role });
+
+	const findUsersByPartialName = (partialName) => {
+		const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+		return model.find({
+			$or: [
+				{ firstName: { $regex: regex } },
+				{ lastName: { $regex: regex } },
+			],
+		});
+	};
+	const findAllUsers = () => model.find();
+
+	const findUserById = (userId) => model.findById(userId);
+
 	const findUserByUsername = (username) =>
-		users.find((user) => user.username === username);
+		model.findOne({ username: username });
+
 	const findUserByCredentials = (username, password) =>
-		users.find(
-			(user) => user.username === username && user.password === password
-		);
+		model.findOne({ username, password });
+
 	const updateUser = (userId, user) =>
-		(users = users.map((u) => (u._id === userId ? user : u)));
-	const deleteUser = (userId) =>
-		(users = users.filter((u) => u._id !== userId));
+		model.updateOne({ _id: userId }, { $set: user });
+
+	const deleteUser = (userId) => model.findByIdAndDelete(userId);
 	return {
 		createUser,
+		findUsersByRole,
+		findUsersByPartialName,
 		findAllUsers,
 		findUserById,
 		findUserByUsername,
@@ -29,5 +43,3 @@ function UsersDao(db) {
 		deleteUser,
 	};
 }
-
-export default UsersDao;
